bb06f53ea5234003c0802f6774eb065d9f0a5119
ksl9508 <39768190+ksl9508@users.noreply.github.com>
2022-05-21 11:53:05 +0800

    Initial commit

---
+++ packages/renderer/src/store/useStore.ts
@@ -0,0 +1,19 @@
+import create from 'zustand';
+import { devtools } from 'zustand/middleware';
+import { combine } from 'zustand/middleware';
+import { storeBears } from './storeBears';
+import { storeUI } from './storeUI';
+
+export const useStore = create(
+  devtools(
+    combine(
+      {
+        hackedBy: 'Blade',
+      },
+      (set:any)=> ({
+        ui: storeUI(set),
+        example: storeBears(set)
+      })
+    )
+  )
+);
